6984e2288fd57c35a7f4464ac0d2b81bcc014ad8
Luxury-Karma <alexgauv@hotmail.ca>
2022-12-02 14:28:44 -0500

    able to take png from a specified folder fuse them save it as final.png

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from PIL import Image
-
+import aspose.words as aw
 
 
 def get_picture():
@@ -12,7 +12,7 @@ def get_picture():
         #check if we are in folder or if it is a file
         if os.path.isfile(os.path.join(img_path,path)):
             files.append(path) #get every files in the folder, ignore all other folder
-    creat_new_img(open_all_file(Separate_files_by_typed(files,img_type_to_get,img_path),img_path))
+    return creat_new_img(open_all_file(Separate_files_by_typed(files,img_type_to_get,img_path),img_path),img_path)
 #    print(f'absolute Path : {img} ,\nimage name : {img_type_to_get},\n folder path : {img_path}')
 #    img_file = open(img,'r')
 #    print(f'file: {img_type_to_get} is Open')
@@ -31,7 +31,7 @@ def Separate_files_by_typed(files, type,path):
         file_to_oppen = []
     for e in file_typed:
         file_to_oppen.append(path+'\\'+e)
-    creat_new_img(file_to_oppen)
+    creat_new_img(file_to_oppen,path)
     return file_typed
 
 
@@ -42,12 +42,39 @@ def open_all_file(files_to_oppen,files_path):
         file_Oppen.append(open(path))
     return file_Oppen
 
-def creat_new_img(file_oppen):
+def creat_new_img(file_oppen,path):
+
+    #oppen all the image
+    img = []
     for e in file_oppen:
-        img = Image.open(e)
-        r,g,b=img.split()
-        img = Image.merge("RGB",(b,g,r))
-        img.show()
+        print(e)
+        f = Image.open(e)
+        img.append(f)
+    #find the size of all image
+    img_size = []
+    for e in img:
+       img_size.append(e.size)
+    #creat a new image with the correct size
+    length = 0
+    for e in img_size:
+        length = length + e[1]
+    new_img = Image.new('RGB', (img_size[0][0], length),(250,250,250))
+    emplacementX= 0
+    emplacementY= 0
+    #place the file where they should be to be past
+    for e in range(len(img)):
+        new_img.paste(img[e],(emplacementY,emplacementX))
+        emplacementX = emplacementX + img_size[e][1]
+    #save the image
+    path_and_name = path+'\\final.PNG'
+    new_img.save(path_and_name, 'PNG')
+    new_img.show()
+    return path_and_name
+
+def main():
+    to_convert = get_picture()
 
 
-get_picture()
+
+
+main()
